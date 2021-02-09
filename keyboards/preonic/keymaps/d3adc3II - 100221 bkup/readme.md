c5f5abdc621629f5a06e1f212054a801dd7bcd6a
d3adc3II <phong.dinh@outlook.com>
2021-02-10 07:15:00 +0800

    Update layout 11-02-2021

---
+++ keyboards/preonic/keymaps/d3adc3II - 100221 bkup/readme.md
@@ -0,0 +1,1 @@
+# 10 Feb Update
---
+++ keyboards/preonic/keymaps/d3adc3II - Copy/config.h
@@ -0,0 +1,53 @@
+#pragma once
+#define NO_ACTION_MACRO
+#define NO_ACTION_FUNCTION
+#define TAPPING_TERM 175
+#define IGNORE_MOD_TAP_INTERRUPT
+
+#define LEADER_PER_KEY_TIMING
+#define LEADER_TIMEOUT 350
+
+#define GRAVE_ESC_ALT_OVERRIDE
+
+#ifdef AUDIO_ENABLE
+    #define STARTUP_SONG SONG(ROCK_A_BYE_BABY)
+   // #define STARTUP_SONG SONG(STARTUP_SONG)
+    // #define STARTUP_SONG SONG(NO_SOUND)
+
+    #define DEFAULT_LAYER_SONGS { SONG(QWERTY_SOUND), \
+                                  SONG(DVORAK_SOUND) \
+                                }
+#endif
+
+#define MUSIC_MASK (keycode != KC_NO)
+
+/*
+ * MIDI options
+ */
+
+/* Prevent use of disabled MIDI features in the keymap */
+//#define MIDI_ENABLE_STRICT 1
+
+/* enable basic MIDI features:
+   - MIDI notes can be sent when in Music mode is on
+*/
+
+#define MIDI_BASIC
+
+/* enable advanced MIDI features:
+   - MIDI notes can be added to the keymap
+   - Octave shift and transpose
+   - Virtual sustain, portamento, and modulation wheel
+   - etc.
+*/
+//#define MIDI_ADVANCED
+
+/* override number of MIDI tone keycodes (each octave adds 12 keycodes and allocates 12 bytes) */
+//#define MIDI_TONE_KEYCODE_OCTAVES 2
+
+#ifndef NO_DEBUG
+#define NO_DEBUG
+#endif // !NO_DEBUG
+#if !defined(NO_PRINT) && !defined(CONSOLE_ENABLE)
+#define NO_PRINT
+#endif // !NO_PRINT
