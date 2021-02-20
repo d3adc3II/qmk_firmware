e8afdc398164237a7b0c56b0a471da145aabedc7
d3adc3II <phong.dinh@outlook.com>
2021-02-21 02:51:21 +0800

    21022021 layout

--- keyboards/preonic/keymaps/d3adc3II/keymap.c
+++ keyboards/preonic/keymaps/d3adc3II/keymap.c
@@ -46,7 +46,7 @@ enum {
   KC_MUTE,    KC_1,    KC_2,    KC_3,        KC_4,    KC_5,     KC_6,    KC_7,     KC_8,    KC_9,    KC_0,    KC_BSPC,
   KC_TAB,     KC_Q,    KC_W,    KC_E,        KC_R,    KC_T,     KC_Y,    KC_U,     KC_I,    KC_O,    KC_P,    KC_LEAD,
   KC_GESC,    KC_A,    KC_S,    KC_D,        KC_F,    KC_G,     KC_H,    KC_J,     KC_K,    KC_L,    KC_SCLN, KC_ENT,
-  KC_LSFT,    KC_Z,    KC_X,    KC_C,        KC_V,    KC_B,     KC_N,    KC_M,     KC_COMM, KC_DOT,  KC_UP, TT(_RAISE),
+  KC_LSFT,    KC_Z,    KC_X,    KC_C,        KC_V,    KC_B,     KC_N,    KC_M,     KC_COMM, KC_DOT,  KC_UP, OSL(_RAISE),
   KC_LCTL,    _______, KC_LGUI, KC_LALT,     LOWER,   KC_SPC,   KC_SPC,  RAISE,    KC_SLSH, KC_LEFT, KC_DOWN, KC_RGHT
 ),    
 
@@ -127,7 +127,7 @@ enum {
   _______, _______, _______,  _______, _______, KC_EQL,  S(KC_EQL), _______, _______, _______, _______, _______,
   _______, KC_6,    KC_7,     KC_8,    KC_9,    KC_0,     KC_PMNS, _______, _______, _______, _______, _______,
   _______, KC_1,    KC_2,     KC_3,    KC_4,    KC_5,     KC_PAST, _______, _______, _______, _______, _______,
-  _______, KC_6,    KC_7,     KC_8,    KC_9,    KC_0,     KC_PSLS, KC_CIRC, _______, _______, _______, _______,
+  _______, KC_6,    KC_7,     KC_8,    KC_9,    KC_0,     KC_PSLS, KC_CIRC, _______, _______, _______, XXXXXXX,
   _______, _______, _______,  _______, _______, _______,  _______, _______, _______, _______, _______, _______
 ),
       
